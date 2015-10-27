4f18f4bb95fd6b498c55c08a93a8654bd5d3c675
William Kreamer <bill@cens.io>
2015-10-27 16:42:23 -0400

    wrote a test case

--- cyvlfeat/fisher/tests/fisher_test.py
+++ cyvlfeat/fisher/tests/fisher_test.py
@@ -4,5 +4,16 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 def test_fisher():
-    #TODO IMPLEMENT ME!
-    assert True
+    N = 1000
+    K = 512
+    D = 128
+    
+    x = np.random.uniform(size=(D,N))
+    means = np.random.uniform(size=(D,K))
+    covariances = np.random.uniform(size=(D,K))
+    priors = np.linalg.norm(np.random.uniform(size=(K,)))
+    enc = fisher(x,means,covariances,priors,Verbose=True)
+    
+    expected = 2*K*D
+    observed = len(enc)
+    assert(expected==observed)
