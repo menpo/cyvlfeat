88e7cd45cd6ecc81702b805dd7f8e5ca6f185086
William Kreamer <bill@cens.io>
2015-10-27 16:35:50 -0400

    added empty test case

---
+++ cyvlfeat/fisher/tests/fisher_test.py
@@ -0,0 +1,8 @@
+from __future__ import division
+from cyvlfeat.fisher import fisher
+import numpy as np
+from numpy.testing import assert_allclose
+
+def test_fisher():
+    #TODO IMPLEMENT ME!
+    assert True
