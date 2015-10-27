61afa9764a57e0a4b85aae374a877fd505f446ea
William Kreamer <bill@cens.io>
2015-10-27 18:59:33 -0400

    fix misunderstanding with the setup.py

--- setup.py
+++ setup.py
@@ -41,16 +41,25 @@ if IS_WIN and IS_CONDA:
     
     # On Windows, there is no relative linking against DLLS,
     # so every extension MUST have a copy of the vl.dll next to it.
-    vl_ext_dll_path = op.join('cyvlfeat', 'sift', 'fisher', 'vl.dll')
-    shutil.copy(conda_vl_dll_path, vl_ext_dll_path)
+    vl_sift_dll_path = op.join('cyvlfeat', 'sift', 'vl.dll')
+    shutil.copy(conda_vl_dll_path, vl_sift_dll_path)
+
+    vl_fisher_dll_path = op.join('cyvlfeat', 'fisher', 'vl.dll')
+    shutil.copy(conda_vl_dll_path, vl_fisher_dll_path)
     
 vl_extensions = [
     Extension('cyvlfeat.sift.cysift',
-              [op.join('cyvlfeat', 'sift', 'cysift.pyx', 'fisher', 'cyfisher.pyx')],
+              [op.join('cyvlfeat', 'sift', 'cysift.pyx')],
               include_dirs=include_dirs,
               library_dirs=library_dirs,
               libraries=['vl'], 
-              language='c++')
+              language='c++'),
+    Extension('cyvlfeat.fisher.cyfisher',
+              [op.join('cyvlfeat', 'fisher', 'cyfisher.pyx')],
+              include_dirs=include_dirs,
+              library_dirs=library_dirs,
+              libraries=['vl'], 
+              language='c++'),
 ]
 
 # Grab all the pyx and pxd Cython files for uploading to pypi
