3ac06f6fc3b0aa7826f801534cf43aa85e417620
Patrick Snape <patricksnape@gmail.com>
2016-04-22 13:11:05 +0200

    Fix building on OSX
    Apparently Cython keeps generating @loader_path/libvl.dylib
    which Conda doesn't know how to fix. So we manually do it
    in the build script

--- conda/build.sh
+++ conda/build.sh
@@ -2,8 +2,23 @@
 
 cp -r $RECIPE_DIR/../ .
 
-export CFLAGS="-I$PREFIX/include $CFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  export CFLAGS="-I$PREFIX/include $CFLAGS"
+  export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+fi
+if [ "$(uname -s)" == "Darwin" ]; then
+  export CFLAGS="-I$PREFIX/include -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET $CFLAGS"
+  export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+fi
 
 "$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
+
+if [ "$(uname -s)" == "Darwin" ]; then
+  # For some reason Cython refuses to generate @rpath/vlfeat.dylib - so
+  # we manually do it here
+  find $SP_DIR/cyvlfeat -name "*.so" -print0 | while read -d $'\0' file
+  do
+    install_name_tool -change @loader_path/libvl.dylib @rpath/libvl.dylib $file
+  done
+fi
--- cyvlfeat/kmeans/cykmeans.pyx
+++ cyvlfeat/kmeans/cykmeans.pyx
@@ -1,4 +1,9 @@
 # distutils: language = c
+# Copyright (C) 2007-12 Andrea Vedaldi and Brian Fulkerson.
+# All rights reserved.
+
+# This file is modified from part of the VLFeat library and is made available
+# under the terms of the BSD license.
 import numpy as np
 cimport numpy as np
 cimport cython
